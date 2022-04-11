3e7493929b9bf0c36cabbedaabdee8840d3dd00e
wookyz <marcoscardoso.programmer@gmail.com>
2022-04-11 16:24:33 -0300

    Revert "@Add rss.js class"
    This reverts commit 5cf094c6bb699cd4f9738a900d54ea2e5b14e98b.

--- parsers/rss.js
+++ parsers/rss.js
@@ -1,59 +1,35 @@
-const Me = imports.misc.extensionUtils.getCurrentExtension()
-const GObject = imports.gi.GObject
-const Base = Me.imports.parsers.base
+function getTitles(feed) {
+    let arr = []
 
-const RssParser = GObject.registerClass(
-    class RssParser extends Base.BaseParser {
+    return arr    
+}
 
-        _init(root) {
-            super._init(root)
-            log("RSS 2.0 Parser")
-        }
+function getImages(feed) {
+    let arr = []
+    
+    return arr
+}
 
-        // Parse feed file
-        parse = function () {
-            this._parsePublisher(this._root.childElements[0].childElements)
-        }
+function getDescriptions(feed) {
+    let arr = []
+    
+    return arr
+}
 
+function getTitle(index) {
+    title = ''
 
-        _parsePublisher = function (childElements) {
+    return title
+}
 
-            for (let index = 0; index < childElements.length; index++) {
-                if (childElements[index].name == 'title') {
-                    this.Publisher.Title = childElements[index].text
-                } else if (childElements[index].name == 'link') {
-                    this.Publisher.Link = childElements[index].text
-                } else if (childElements[index].name == 'description') {
-                    this.Publisher.Description = childElements[index].text
-                } else if (childElements[index].name == 'pubDate') {
-                    this.Publisher.PublishDate = childElements[index].text
-                } else if (childElements[index].name == 'item') {
-                    this._parseItem(childElements[index].childElements)
-                }
-            }
-        }
+function getImage(index) {
+    src = ''
 
+    return src
+}
 
-        _parseItem = function (itemElements) {
-            let item = this._initItem()
+function getDescription(index) {
+    description = ''
 
-            for (let index = 0; index < itemElements.length; index++) {
-
-                if (itemElements[index].name == 'title') {
-                    item.Title = itemElements[index].text
-                } else if (itemElements[index].name == 'link') {
-                    item.HttpLink = itemElements[index].text
-                } else if (itemElements[index].name == 'description') {
-                    item.Description = itemElements[index].text
-                } else if (itemElements[index].name == 'author') {
-                    item.Author = itemElements[index].text
-                } else if (itemElements[index].name == 'enclosure') {
-                    item.ImgLink = itemElements[index].attributes.url
-                } else if (itemElements[index].name == 'pubDate') {
-                    item.PublishDate = itemElements[index].text
-                }
-            }
-            this.Items.push(item)
-        }
-
-    })
+    return description
+}
